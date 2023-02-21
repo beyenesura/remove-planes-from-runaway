322195511c55d128a9aa9b35e6235413ff85ebfd
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-21 14:23:12 -0500

    Update Runway.js

--- Runway.js
+++ Runway.js
@@ -10,9 +10,19 @@ class Runway {
     
     add(plane) {
 
-        if (Runway.planes.length>Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
+        if (Runway.planes.length >= Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
             throw new Error ("runways at full capacity!")
         }
         Runway.planes.push(plane)
     }
+
+    remove(plane) {
+        const index = Runway.planes.indexOf(plane);
+        if (index === -1) {
+            throw new Error("plane not found on this runway!");
+        }
+        Runway.planes.splice(index, 1);
+    }
 }
+
+module.exports = Runway;
